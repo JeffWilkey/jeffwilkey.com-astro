a52af7376eeb3db1b805424da4aea26af5a32241
Jeff Wilkey <contact@jeffwilkey.com>
2023-05-27 12:48:53 -0700

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export default {
+  siteTitle: "Jeff Wilkey",
+  siteDescription: "The personal site of Astronaut",
+  brandText: "<JW/>",
+  authorFirstName: "Jeff",
+  authorLastName: "Wilkey",
+  githubUrl: "https://github.com/JeffWilkey",
+  linkedinUrl: "https://www.linkedin.com/in/jeffwilkey/",
+  codepenUrl: "https://codepen.io/JeffWilkey",
+};
